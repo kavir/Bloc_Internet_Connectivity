327f92366d734c560935c187dfe728746af6263a
kavir <kavirmansingh7@gmail.com>
2024-01-02 20:46:28 +0545

    third commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,20 +9,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(
-        child: BlocBuilder<InternetBloc, InternetState>(
+      body: SafeArea(
+          child: Center(
+        child: BlocConsumer<InternetBloc, InternetState>(  //blocconsumer is the collection of bloclistener and blocbuilder
+        
+          listener: (context, state) {
+            if(state is InternetGainedState){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content:Text("Internet Connected"),
+                backgroundColor: Colors.green,));
+            }
+            else if(state is InternetLostState){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content:Text("Internet Disconnected"),
+                backgroundColor: Colors.red,));
+            }
+            
+          },
           builder: (context, state) {
             if(state is InternetGainedState){
               return Text("Connected");
             }
             else if(state is InternetLostState){
-              return Text("Not Connected");
+              return Text("Disconnected");
             }
             else{
               return Text("Loading.. .");
             }
           },
         ),
+
+        
       )),
     );
   }
